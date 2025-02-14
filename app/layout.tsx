721c0343db96a00491ac173af4611b14eed86ca7
Alex <alexwang409@gmail.com>
2025-02-13 22:57:41 -0500

    favicon v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,21 +42,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      {
-        url: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
+      { url: "/icon.png", type: "image/png", sizes: "32x32" },
     ],
     shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    apple: { url: "/apple-icon.png", type: "image/png", sizes: "180x180" },
   },
 };
 
@@ -67,10 +56,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
